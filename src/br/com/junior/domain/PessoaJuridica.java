dfb31cc5e5244efd2c20f1924ebea27dbd30e377
Junior Erdmann Streichan <juniorstreichan@gmail.com>
2018-04-10 22:08:19 -0400

    commit inicial

---
+++ src/br/com/junior/domain/PessoaJuridica.java
@@ -0,0 +1,69 @@
+package br.com.junior.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorColumn(name="PJ")
+public class PessoaJuridica extends Pessoa implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	private String razaoSocial;
+	private String cnpj;
+	private String inscricaoEstadual;
+	private String inscricaoMunicipal;
+	
+	public PessoaJuridica() {
+		super();
+	}
+	
+	public PessoaJuridica(String razaoSocial, String cnpj, String inscricaoEstadual, String inscricaoMunicipal) {
+		super();
+		this.razaoSocial = razaoSocial;
+		this.cnpj = cnpj;
+		this.inscricaoEstadual = inscricaoEstadual;
+		this.inscricaoMunicipal = inscricaoMunicipal;
+	}
+	
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getInscricaoEstadual() {
+		return inscricaoEstadual;
+	}
+
+	public void setInscricaoEstadual(String inscricaoEstadual) {
+		this.inscricaoEstadual = inscricaoEstadual;
+	}
+
+	public String getInscricaoMunicipal() {
+		return inscricaoMunicipal;
+	}
+
+	public void setInscricaoMunicipal(String inscricaoMunicipal) {
+		this.inscricaoMunicipal = inscricaoMunicipal;
+	}
+
+
+	
+	
+	
+   
+}
